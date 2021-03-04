fffafd778a62ea563709a4e9bac7b8ebec6e42ff
lucaf99 <lucafarci99@gmail.com>
2021-03-04 17:07:53 +0100

    Esecuzione query

--- DataReduction.py
+++ DataReduction.py
@@ -3,18 +3,18 @@ import psycopg2 as psycopg2
 conn = psycopg2.connect("dbname=CASAS400 user=postgres password=postgres")
 cur = conn.cursor()
 
-'''try:
-    cur.execute("ALTER TABLE public.events ADD presence bool")
-except psycopg2.DatabaseError as e:
-    print(e)'''
+query = "CREATE VIEW events_no_duplicates AS\
+        SELECT patient,time,sensor,value FROM\
+             (SELECT events.*\
+             FROM (SELECT events.*, LAG(sensor) OVER (ORDER BY patient) AS prev_sensor FROM events\
+             WHERE sensor LIKE 'm%' AND value='on') events\
+        WHERE prev_sensor IS DISTINCT FROM sensor) AS right_info;"
 
-cur.execute("select * from public.events")
-result = cur.fetchall()
-patientOccurence = (result[0])[0]
-#for row in result:
-
-
-
+try:
+    cur.execute(query)
+    print("Eseguito correttamente")
+except psycopg2.DataError as e:
+    print(e)
 
 cur.close()
 conn.commit()
