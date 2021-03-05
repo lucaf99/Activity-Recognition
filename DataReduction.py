df20c405565f1eda182629e00e21c9bc51bbf939
lucaf99 <lucafarci99@gmail.com>
2021-03-05 16:05:42 +0100

    Esecuzione query

--- DataReduction.py
+++ DataReduction.py
@@ -1,21 +1,12 @@
-import psycopg2 as psycopg2
 
-conn = psycopg2.connect("dbname=CASAS400 user=postgres password=postgres")
-cur = conn.cursor()
+def reduceData(cur):
+    query = "CREATE VIEW events_no_duplicates AS\
+            SELECT patient,time,sensor,value FROM\
+                 (SELECT events.*\
+                 FROM (SELECT events.*, LAG(sensor) OVER (ORDER BY patient) AS prev_sensor FROM events\
+                 WHERE sensor LIKE 'm%' AND value='on') events\
+            WHERE prev_sensor IS DISTINCT FROM sensor) AS right_info;"
 
-query = "CREATE VIEW events_no_duplicates AS\
-        SELECT patient,time,sensor,value FROM\
-             (SELECT events.*\
-             FROM (SELECT events.*, LAG(sensor) OVER (ORDER BY patient) AS prev_sensor FROM events\
-             WHERE sensor LIKE 'm%' AND value='on') events\
-        WHERE prev_sensor IS DISTINCT FROM sensor) AS right_info;"
-
-try:
     cur.execute(query)
     print("Eseguito correttamente")
-except psycopg2.DatabaseError as e:
-    print(e)
 
-cur.close()
-conn.commit()
-conn.close()
---
+++ main.py
@@ -0,0 +1,26 @@
+import psycopg2 as psycopg2
+
+from DataReduction import reduceData
+from config import config
+
+conn = None
+
+try:
+    params = config()
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    print(db_version)
+
+    reduceData(cur)  # Funzione riduzione numero dati
+
+    cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
